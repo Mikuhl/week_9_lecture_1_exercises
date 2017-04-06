74449593af74bc70e6eb94c4668844c5519760ee
mikuhl <mikuhl@comcast.net>
2017-04-05 20:03:38 -0500

    first commit

---
+++ ex1.js
@@ -0,0 +1,14 @@
+//Write a jQuery statement that selects all <h2> tags
+$("h2")
+
+//Write a jQuery statement that selects the tag with ID="first"
+$("#first")
+
+//Using jQuery, create a click event on a button with ID of 'hidediv' that will hide a div tag with an ID of 'hideme'.
+//(For reference, jQuery has a .hide() method)
+$("#hidediv").click(function() {
+  .hide("hideme");
+});
+
+//Using jQuery, create a click event on a button with ID of 'hidediv' that will hide a div tag with an ID of 'hideme'.
+//(For reference, jQuery has a .hide() method)
